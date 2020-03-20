46a6c8bb9c5d61dba860021a736e7be49b239890
saefulimam <56109440+saefulimam@users.noreply.github.com>
2020-03-20 15:14:44 +0700

    Add files via upload

---
+++ tugas1/src/mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class mahasiswa {
+    String nama;
+    String kelas;
+    int nim;
+    
+    public static void main(String[] args) {
+        mahasiswa objek = new mahasiswa();
+        System.out.println(objek.nama = "Saeful Imam Gunawan");
+        System.out.println(objek.kelas = "4A");
+        System.out.println(objek.nim = 18090039);
+        
+    }
+}
